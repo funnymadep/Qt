b8287a26d55193068cca3583990abb1716df99f8
hql <2454618038@qq.com>
2024-07-11 18:04:57 +0800

    rm build

--- qt_finish/hit_Qt/build/CMakeFiles/hit_menu.dir/src/sysLogWidegt.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/hit_menu.dir/src/sysLogWidegt.cpp.o: \
- /home/sd-robot/hql_test/hit_Qt/src/sysLogWidegt.cpp \
- /usr/include/stdc-predef.h
--- qt_finish/hit_Qt/build/CMakeFiles/hit_menu.dir/src/sysSettingWidget.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/hit_menu.dir/src/sysSettingWidget.cpp.o: \
- /home/sd-robot/hql_test/hit_Qt/src/sysSettingWidget.cpp \
- /usr/include/stdc-predef.h
